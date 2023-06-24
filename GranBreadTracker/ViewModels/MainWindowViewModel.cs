f43aa2730772c56ee02fedc9bbcf227487661f2a
NahFarn <nathan.singh@inreachsw.com>
2023-06-24 17:11:40 +1000

    Start avalonia ui

---
+++ GranBreadTracker/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace GranBreadTracker.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ GranBreadTracker/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GranBreadTracker.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
