e58918985014ffcc1312832fbfa67406330ef510
NahFarn <nathan.singh@inreachsw.com>
2023-07-01 17:36:39 +1000

    Add Item Source handler

--- GranBreadTracker/ViewModels/ItemTrackerPageViewModel.cs
+++ GranBreadTracker/ViewModels/ItemTrackerPageViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.ObjectModel;
 using DynamicData;
+using FluentAvalonia.UI.Controls;
 using GranBreadTracker.Classes;
+using GranBreadTracker.Pages;
 
 namespace GranBreadTracker.ViewModels;
 
@@ -14,11 +16,63 @@ public class ItemTrackerPageViewModel : ViewModelBase
 
     public ItemTrackerPageViewModel(ItemTrackerDef trackerDef)
     {
+        AddItemSourceCommand = new GeneralCommand(AddItemSoureExecute);
+
         Sources = new ObservableCollection<ItemSourcePageViewModel>();
 
         ItemTrackerDef = trackerDef;
 
         var sourceVms = ItemTrackerDef.GenerateSourceViewModels();
         Sources.AddRange(sourceVms);
+    }
+    
+    private void AddItemSoureExecute(object obj)
+    {
+        // Pop up item tracker creation dialog 
+        CreateNewItemSource();
+    }
+    
+    private async void CreateNewItemSource()
+    {
+        var dialog = new ContentDialog
+        {
+            Title = "New Item Source",
+            PrimaryButtonText = "Create",
+            CloseButtonText = "Cancel"
+        };
+
+        // Pass the dialog if you need to hide it from the ViewModel.
+        var viewModel = new NewItemTrackerDialogViewModel(dialog);
+
+        // In our case the Content is a UserControl, but can be anything.
+        dialog.Content = new NewItemTrackerDialog()
+        {
+            DataContext = viewModel
+        };
+
+        var dialogResult = await dialog.ShowAsync();
+        
+        if (dialogResult == ContentDialogResult.Primary)
+        {
+            var newItemDialog = dialog.Content as NewItemTrackerDialog;
+            var newItemViewModel = newItemDialog.DataContext as NewItemTrackerDialogViewModel;
+            var itemName = newItemViewModel.ItemName;
+            if (!string.IsNullOrEmpty(itemName))
+            {
+                var iconSource = newItemViewModel.Icon;
+                var returnDef = new ItemSourceDef
+                {
+                    Name = itemName,
+                    IconSource = iconSource,
+                    Description = "New Item Source, rename me, give an icon customise Drop Locations.",
+                };
+                
+                var newVm = new ItemSourcePageViewModel(returnDef);
+                Sources.Add(newVm);
+            }
+           
+        }
+        
+        //return returnDef;
     }
 }
